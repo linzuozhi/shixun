8fa28ca3dcafefe7d1e99e5971d8746eb7cbc268
linzuozhi <1159461120@qq.com>
2023-06-05 19:42:23 +0800

    6.5-4

--- src/components/pagecomponents/login.vue
+++ src/components/pagecomponents/login.vue
@@ -87,7 +87,7 @@ export default {
 
   },
   methods: {
-    
+
     initSendData() {
       this.sendData.id = this.jsonData.id;
       this.sendData.title = this.jsonData.title;
--- src/components/pagecomponents/result.vue
+++ src/components/pagecomponents/result.vue
@@ -4,8 +4,8 @@
     <el-button icon="el-icon-search" circle @click="submitLink"></el-button>
     <h2>{{ dataofwritten.titleofquestionnaire }}</h2>
     <div v-for="question in dataofwritten.questiondata" :key="question.id">
-      <h3 v-if="question.type === 1">{{ question.title }}</h3>
-      <div v-if="question.type === 1" class="chart-container">
+      <h3 v-if="question.type === 1 || question.type === 0">{{ question.title }}</h3>
+      <div v-if="question.type === 1 || question.type === 0" class="chart-container">
         <div :id="'chart' + question.id" style="width: 400px; height: 400px;"></div>
       </div>
       <h3 v-if="question.type === 2">{{ question.title }}</h3>
@@ -20,86 +20,19 @@
 
 <script>
 import * as echarts from 'echarts';
-
+import axios from 'axios';
 export default {
   data() {
     return {
       jsonData:{},
       inputLink:"",
       dataofwritten: {
-        titleofquestionnaire: '问卷标题',
-        questiondata: [
-          {
-            type: 1,
-            title: "单选题1",
-            id: 0,
-            seleted: [
-              {
-                id: 0,
-                textofoption: '选项1',
-                num: 5,
-              },
-              {
-                id: 1,
-                textofoption: '选项2',
-                num: 3,
-              },
-              {
-                id: 2,
-                textofoption: '选项3',
-                num: 6,
-              },
-              {
-                id: 3,
-                textofoption: '选项4',
-                num: 2,
-              },
-            ],
-          },
-          {
-            type: 1,
-            title: "单选题2",
-            id: 1,
-            seleted: [
-              {
-                id: 0,
-                textofoption: '选项1',
-                num: 5,
-              },
-              {
-                id: 1,
-                textofoption: '选项2',
-                num: 3,
-              },
-              {
-                id: 2,
-                textofoption: '选项3',
-                num: 6,
-              },
-              {
-                id: 3,
-                textofoption: '选项4',
-                num: 2,
-              },
-              {
-                id: 4,
-                textofoption: '选项5',
-                num: 1
-              }
-            ],
-          },
-          {
-            type: 2,
-            id: 2,
-            title: "填空题",
-            filldata: ['填空回答1', '填空回答2', '填空回答3'],
-          },
-        ],
+
       },
     };
   },
   mounted() {
-    this.renderCharts();
+   
   },
   methods: {
     submitLink() {
@@ -109,9 +42,9 @@ export default {
               alert('链接无效');
               this.inputValue = '';
             } else {
-              this.jsonData = response.data;
+              this.dataofwritten = response.data;
               this.renderCharts();
-              // 记得改renderCharts里的this.dataofwritten.questiondata为this.jsonData.questiondata
+              
             }
           })
           .catch(error => {
@@ -120,7 +53,7 @@ export default {
     },
     renderCharts() {
       for (const question of this.dataofwritten.questiondata) {
-        if (question.type === 1) {
+        if (question.type === 1 || question.type === 0) {
           const chartId = `chart${question.id}`;
           const chartData = question.seleted.map((option) => ({
             name: option.textofoption,
